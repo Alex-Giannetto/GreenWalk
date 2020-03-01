8649c509d0aefe7c2f4695acd0a6f64aa67d9669
Alexg78bis <alexg78bis@gmail.com>
2020-03-01 15:22:22 +0100

    http get method

--- src/app/requests/request/request.service.ts
+++ src/app/requests/request/request.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
 
 @Injectable({
 	providedIn: 'root'
 })
 export class RequestService {
 
-	constructor () { }
+	constructor (private httpClient: HttpClient) { }
+
+	get (url): Observable<object> {
+		return this.httpClient.get(url)
+	}
 }
