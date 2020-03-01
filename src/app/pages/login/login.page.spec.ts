56909c5c8342b6e0b55b8947bf7253f91cc3b8a9
Alexg78bis <alexg78bis@gmail.com>
2020-03-01 14:26:42 +0100

    testing

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -2,20 +2,40 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { IonicModule } from '@ionic/angular'
 
 import { LoginPage } from './login.page'
+import { DebugElement } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { By } from '@angular/platform-browser'
 
 describe('LoginPage', () => {
 	let component: LoginPage
 	let fixture: ComponentFixture<LoginPage>
 
+	let elements: {
+		mail: DebugElement,
+		password: DebugElement,
+		submit: DebugElement
+	}
+
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [LoginPage],
-			imports: [IonicModule.forRoot()]
+			imports: [
+				IonicModule.forRoot(),
+				ReactiveFormsModule,
+				FormsModule
+			]
 		}).compileComponents()
 
 		fixture = TestBed.createComponent(LoginPage)
 		component = fixture.componentInstance
+		component.ngOnInit()
 		fixture.detectChanges()
+
+		elements = {
+			mail: fixture.debugElement.query(By.css('input[type=email]')),
+			password: fixture.debugElement.query(By.css('input[type=password]')),
+			submit: fixture.debugElement.query(By.css('ion-button[type=submit]'))
+		}
 	}))
 
 	it('should create', () => {
