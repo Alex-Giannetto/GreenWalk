3ef7bae1c3a4545d9e6525b31422d774f94d91cf
Alexg78bis <alexg78bis@gmail.com>
2020-03-08 18:22:28 +0100

    register form

---
+++ src/app/forms/register.form.ts
@@ -0,0 +1,34 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+
+export const RegisterForm: FormGroup = new FormGroup({
+	step1: new FormGroup({
+		firstName: new FormControl(null, [
+			Validators.required,
+			Validators.email
+		]),
+		email: new FormControl(null, [
+			Validators.required,
+			Validators.email
+		]),
+		birthDate: new FormControl(null, [
+			Validators.required,
+		]),
+	}),
+
+	step2: new FormGroup({
+		password: new FormControl(null, [
+			Validators.required,
+			Validators.minLength(8)
+		]),
+		repeatPassword: new FormControl(null, [
+			Validators.required,
+			Validators.minLength(8)
+		]),
+	}),
+
+	step3: new FormGroup({
+		cgu: new FormControl(null, [
+			Validators.required,
+		])
+	})
+})
