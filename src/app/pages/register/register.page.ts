487f5843e2282ae4cfe7dc3178999f1cb63a2b3e
Alexg78bis <alexg78bis@gmail.com>
2020-03-08 18:30:42 +0100

    register step 1 integration

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+import { FormGroup } from '@angular/forms'
+import { RegisterForm } from '../../forms/register.form'
 
 @Component({
 	selector: 'app-registration',
 	templateUrl: './register.page.html',
 	styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+
+	formGroup: FormGroup = RegisterForm
 
 	constructor () { }
-
-	ngOnInit () {
-	}
 
 }
