27b8574579be60082e915b6aa1e996a92c756b28
Alexg78bis <alexg78bis@gmail.com>
2020-03-19 18:29:23 +0100

    ma service testing

--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -10,7 +10,7 @@ export class MapService {
 		return `https://api.mapbox.com/styles/v1/mapbox/${this.getStyle()}/static/pin-s-circle+669d34(${location.longitude},${location.latitude})/${location.longitude},${location.latitude},10,0/${size.width}x${size.height}@2x?access_token=${environment.mapBox.token}&attribution=false&logo=false`
 	}
 
-	private getStyle (): string {
+	getStyle (): string {
 		console.log('is dark ?', window.matchMedia('(prefers-color-scheme: dark)').matches)
 		return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-v10' : 'streets-v11'
 	}
