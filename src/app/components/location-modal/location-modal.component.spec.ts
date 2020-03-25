7c45139fc11cca93b9084d50959aa977061bafc5
Alexg78bis <alexg78bis@gmail.com>
2020-03-25 20:05:25 +0100

    add button in header to manual Testing

--- src/app/components/location-modal/location-modal.component.spec.ts
+++ src/app/components/location-modal/location-modal.component.spec.ts
@@ -1,24 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { IonicModule, ModalController } from '@ionic/angular'
 
-import { LocationModalComponent } from './location-modal.component';
+import { LocationModalComponent } from './location-modal.component'
+import { GeolocationService } from '../../services/geolocation/geolocation.service'
+
+const Mock = {
+	geolocationService: () => {}
+}
 
 describe('LocationModalComponent', () => {
-  let component: LocationModalComponent;
-  let fixture: ComponentFixture<LocationModalComponent>;
+	let component: LocationModalComponent
+	let fixture: ComponentFixture<LocationModalComponent>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LocationModalComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [LocationModalComponent],
+			providers: [
+				{ provide: GeolocationService, useValue: Mock.geolocationService() },
+				ModalController
+			],
+			imports: [IonicModule]
+		}).compileComponents()
 
-    fixture = TestBed.createComponent(LocationModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+		fixture = TestBed.createComponent(LocationModalComponent)
+		component = fixture.componentInstance
+		fixture.detectChanges()
+	}))
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+	it('should create', () => {
+		expect(component).toBeTruthy()
+	})
+})
