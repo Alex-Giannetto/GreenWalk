702e639bb596ab21edc0ea561a8bd8dc0e381aa5
alexg78bis <alexg78bis@gmail.com>
2020-05-23 21:36:30 +0200

    add GreenWalk form

---
+++ src/app/forms/GreenWalk/add.form.ts
@@ -0,0 +1,31 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+
+export const AddGreenWalkForm: FormGroup = new FormGroup({
+	name: new FormControl(null, [
+		Validators.required,
+		Validators.maxLength(255),
+	]),
+	datetime: new FormControl(null, [
+		Validators.required,
+	]),
+	description: new FormControl(null, [
+		Validators.required,
+	]),
+	location: new FormGroup({
+		longitude: new FormControl(null, [
+			Validators.required,
+		]),
+		latitude: new FormControl(null, [
+			Validators.required,
+		]),
+		city: new FormControl(null, [
+			Validators.required,
+		]),
+		zipCode: new FormControl(null, [
+			Validators.required,
+		]),
+		street: new FormControl(null, [
+			Validators.required,
+		]),
+	}),
+})
