ead101cc8160d90ed498840b26d6419c0a7c1bbb
alexg78bis <alexg78bis@gmail.com>
2020-06-29 17:45:09 +0200

    fix query when expired token

--- src/app/requests/request.ts
+++ src/app/requests/request.ts
@@ -5,38 +5,44 @@ import { environment } from '../../environments/environment'
 import { LocalService } from '../services/local/local.service'
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class Request {
 
-	constructor (private httpClient: HttpClient) { }
+  constructor (private httpClient: HttpClient) { }
 
-	get (url: string, headers: { [key: string]: string } = null, prefixUrl: boolean = true): Observable<object> {
-		url = prefixUrl ? environment.api.url + url : url
-		return this.httpClient.get(url, this.getHeader(headers))
-	}
+  get (
+    url: string, headers: { [key: string]: string } = null,
+    prefixUrl: boolean = true, withToken: boolean = true): Observable<object> {
+    url = prefixUrl ? environment.api.url + url : url
+    return this.httpClient.get(url, this.getHeader(headers, withToken))
+  }
 
-	post (
-		url: string, data: object, headers: { [key: string]: string } = null,
-		prefixUrl: boolean = true): Observable<object> {
-		url = prefixUrl ? environment.api.url + url : url
-		return this.httpClient.post(url, data, this.getHeader(headers))
-	}
+  post (
+    url: string, data: object, headers: { [key: string]: string } = null,
+    prefixUrl: boolean = true,
+    withToken: boolean = true,
+  ): Observable<object> {
+    url = prefixUrl ? environment.api.url + url : url
+    return this.httpClient.post(url, data, this.getHeader(headers, false))
+  }
 
-	getHeader (externalHeader: { [key: string]: string }): { headers: HttpHeaders } {
-		const token = LocalService.token
+  getHeader (
+    externalHeader: { [key: string]: string },
+    withToken = true): { headers: HttpHeaders } {
+    const token = LocalService.token
 
-		const xAuthTokenHeader = token ? {
-			'X-AUTH-TOKEN': token,
-		} : {}
-		const headers = new HttpHeaders({
-			...xAuthTokenHeader,
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
-			Accept: 'application/json',
-			'content-type': 'application/json',
-			...externalHeader,
-		})
-		return { headers }
-	}
+    const xAuthTokenHeader = withToken && token ? {
+      'X-AUTH-TOKEN': token,
+    } : {}
+    const headers = new HttpHeaders({
+      ...xAuthTokenHeader,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
+      Accept: 'application/json',
+      'content-type': 'application/json',
+      ...externalHeader,
+    })
+    return { headers }
+  }
 }
